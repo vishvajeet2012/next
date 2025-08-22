fc46ccf966496dfd237c68b7a757d8a463461eb1
vishvajeet <djritikshukla@gmail.com>
2025-08-22 17:47:11 +0530

    vish

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,10 +1,24 @@
 export default function NavBar(){
 
+        const navLinks = [
+              {  navName:"SearchFunction" , link:"search"},
+                        {        navName:"counter" , link:"counter"},
+
+
+        ]
+
     return (
         <>  
-    <header>
-            <nav>
-            <
+    <header className="text-white bg-white ">
+            <nav className=" flex gap-4  text-black " >
+            { navLinks&& navLinks?.map((value , index)=>(
+
+            
+            <ul className=" cursor-pointer p-2" key={index}>
+                    <li > 
+                       { value.navName}
+                    </li>
+            </ul>         ) )  }
 
 
 
